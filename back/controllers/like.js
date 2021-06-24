e6900a08d51e58932cc748fdcd6471a59b7e7383
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-24 12:21:33 +0200

    Ajout route Like

--- back/controllers/like.js
+++ back/controllers/like.js
@@ -1,52 +1,35 @@
-const Sauce = require('../models/sauce');
+const Like = require('../database/models/').sequelize.models.Likes;
 
 const logger = require('../logger');
 
 
 
 exports.like = (req, res, next) => {
-    // recherche de la sauce
-    Sauce.findOne({_id: req.params.id})
-    .then(
-      (sauce) => {
-        // Like et pas déja liké/disliké
-        if(req.body.like === 1 && ! sauce.isLiked(req.body.userId) && ! sauce.isDisliked(req.body.userId) ){
-          sauce.like(req.body.userId);
-          Sauce.updateOne({ _id: sauce._id }, sauce)
-          .then(() => res.status(200).json({ message: 'Item liked !'}))
-          .catch(error => res.status(400).json({ error }));
-        }
-        // Dislike et pas déja liké/disliké
-        else if(req.body.like === -1 && ! sauce.isLiked(req.body.userId) && ! sauce.isDisliked(req.body.userId) ){
-          sauce.dislike(req.body.userId);
-          Sauce.updateOne({ _id: sauce._id }, sauce)
-          .then(() => res.status(200).json({ message: 'Item disliked !'}))
-          .catch(error => res.status(400).json({ error }));
-        }
-        // Unlike/Undislike et déja liké
-        else if(req.body.like === 0 && sauce.isLiked(req.body.userId) ){
-          sauce.unLike(req.body.userId);
-          Sauce.updateOne({ _id: sauce._id }, sauce)
-          .then(() => res.status(200).json({ message: 'Item unliked !'}))
-          .catch(error => res.status(400).json({ error }));
-        }
-        // Unlike/Undislike et déja disliké
-        else if(req.body.like === 0 && sauce.isDisliked(req.body.userId) ){
-          sauce.unDislike(req.body.userId);
-          Sauce.updateOne({ _id: sauce._id }, sauce)
-          .then(() => res.status(200).json({ message: 'Item undisliked !'}))
-          .catch(error => res.status(400).json({ error }));
-        }
-        // Autres cas : requette impossible à traiter
-        else{
-          res.status(400).json({error: "Bad request"});
-        }
-      }
-    ).catch(
-      (error) => {
-        res.status(404).json({
-          error: error
-        });
-      }
-    );
+  // On essaie d'insérer un nouveau like
+  Like.create({
+    liked: req.body.liked,
+    userId: req.body.userId,
+    articleId: req.params.articleId
+  })
+  .then(() => res.status(201).json({ message: 'Like saved !' }))
+  .catch(error => {
+    // En cas d'erreur car le like existe déja, on essaie d'update la ligne existante
+    if (error.name === "SequelizeUniqueConstraintError"){
+      Like.update({ liked: req.body.liked }, { where: { id: req.body.userId, articleId: req.params.articleId } })
+      .then(() => res.status(200).json({ message: 'Like saved !'}))
+      .catch(error => res.status(400).json({ error }));
+    } else {
+      res.status(400).json({ error });
+    }
+  });
 };
+
+
+
+
+
+
+
+
+
+
