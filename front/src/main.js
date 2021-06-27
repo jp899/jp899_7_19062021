d50d753b88dc07ff644c761b17282a92c24f311a
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-27 16:00:27 +0200

    Installation bootstrap

--- front/src/main.js
+++ front/src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Import modules boostrapvue et icones bootstrap
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App).use(store).use(router).use(BootstrapVue).use(IconsPlugin).mount('#app')
