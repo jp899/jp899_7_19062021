d4da6c73db8675edfb5464887fb4d01b9240b245
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-27 17:47:24 +0200

    Switch vers Vue V2 car Bootstrap-vue pas compatible avec Vue V3

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,4 +11,14 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).use(store).use(router).use(BootstrapVue).use(IconsPlugin).mount('#app')
+// createApp(App).use(store).use(router).use(BootstrapVue).use(IconsPlugin).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  BootstrapVue,
+  IconsPlugin,
+  render: h => h(App)
+}).$mount('#app')
