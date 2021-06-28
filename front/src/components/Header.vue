5ff850bb628b3719fa727c2b7ec6e54ed80cf16d
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-06-28 17:59:02 +0200

    Menu nav ajout logout

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -18,28 +18,11 @@
                 <b-nav-item v-show="!withProfile"><router-link class="text-decoration-none" to="/signup">S'inscrire</router-link></b-nav-item>
                 <b-nav-item-dropdown v-show="withProfile" text="User" right>
                   <b-dropdown-item><router-link class="text-decoration-none" to="/profile">Mon profil</router-link></b-dropdown-item>
-                  <b-dropdown-item><router-link class="text-decoration-none" to="/login">Se déconnecter</router-link></b-dropdown-item>
+                  <b-dropdown-item @click="logout">Se déconnecter</b-dropdown-item>
                 </b-nav-item-dropdown>
             </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <!-- <b-navbar-nav >
-          
-                <b-nav-item-dropdown text="User" right>
-                  <b-dropdown-item><router-link class="text-decoration-none" to="/profile">Mon profil</router-link></b-dropdown-item>
-                  <b-dropdown-item><router-link class="text-decoration-none" to="/login">Se déconnecter</router-link></b-dropdown-item>
-                </b-nav-item-dropdown>
-
-                <b-nav-item-dropdown right> -->
-                  <!-- Using 'button-content' slot -->
-                  <!-- <template #button-content>
-                      <em>User</em>
-                  </template>
-                  <b-dropdown-item><router-link class="text-decoration-none" to="/profile">Mon profil</router-link></b-dropdown-item>
-                  <b-dropdown-item><router-link class="text-decoration-none" to="/login">Se déconnecter</router-link></b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-navbar-nav> -->
           </b-collapse>
+
         </b-navbar>
       </nav>
     </header>
