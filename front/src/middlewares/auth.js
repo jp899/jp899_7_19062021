8e21fd86cd6819f72e84b3bf1dbc012b9fedad08
Jean-Paul Navailles <navailles.jp3@gmail.com>
2021-07-31 18:24:23 +0200

    Gestion erreur lorsque redirection vers login si déja en page login

--- front/src/middlewares/auth.js
+++ front/src/middlewares/auth.js
@@ -1,8 +1,18 @@
 // Middleware qui redirige vers la route Login si l'utilisateur n'est pas connecté
 export default function auth({ next, router }) {
-    if (! (JSON.parse(localStorage.getItem('user')).id && localStorage.getItem('userToken')) ){
-        console.log('test')
-        return router.push({ name: 'Login' });
+    if (! ( localStorage.getItem('user')
+            && JSON.parse(localStorage.getItem('user')).id 
+            && localStorage.getItem('userToken')) ){
+        return router.push({ name: 'Login' }).catch(err => {
+            // Ignorer l'erreur de redirection vers Login lorsque l'on est déja sur Login.
+            if (
+              err.name !== 'NavigationDuplicated' &&
+              !err.message.includes('Avoided redundant navigation to current location')
+            ) {
+              // But print any other errors to the console
+              console.error(err);
+            }
+          });
     }
     return next();
   }
